283fb492d164d558cab253c5dbd3234859d9162b
= <=>
2024-06-04 13:22:09 +0200

    Mini Calculator built using functions. Syntax is just different.

--- basics/functions.py
+++ basics/functions.py
@@ -1,28 +1,30 @@
 import math
 
-def addition(num_one = 0, num_two = 0):
+def addition(num_one, num_two):
     return num_one + num_two
 
-def addition(num_one = 0, num_two = 0):
+def subtraction(num_one, num_two):
     return num_two - num_one
 
-def division(num_one = 0, num_two = 0):
+def division(num_one, num_two):
     return math.floor(num_two/num_one)
 
-def multiplication(num_one = 0, num_two = 0):
-    return math.floor(num_two/num_one)
-
-# def calculate(operation, num_one, num_two):
-#     operation:
-#         case 1:
-#             return addition(num_one, num_two)
-
-RUN_CALCULATOR = True
+def multiplication(num_one, num_two):
+    return num_two*num_one
 
 num_one = int(input("Enter first number:"))
 num_two = int(input("Enter second number:"))
 
-operation = (input("\n Enter Operation: \n 1. Addition (+) \n 2. Subtraction (-) \n 3. Multiplication (*) \n 4. Division (+) \n Please enter numbers only (1 to 4)."))
+operation = int(input("\n Enter Operation: \n 1. Addition (+) \n 2. Subtraction (-) \n 3. Multiplication (*) \n 4. Division (+) \n Please enter numbers only (1 to 4)."))
 
-
-    
+if operation == 1:
+    print(addition(num_one, num_two))
+elif operation == 2:
+    print(subtraction(num_two, num_one))
+elif operation == 3:
+    print(division(num_two, num_one))
+elif operation == 4:
+     print(multiplication(num_two, num_one))
+else:
+    print("Please select valid operation.")
+ 
